215af2e075aed1248e24a8d4eebcc629fdc99d28
Krishnakumar Chandran <mail2krishnachandran@gmail.com>
2024-10-18 23:34:56 -0700

    Concurrency explained with go routines

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"go-workshop/basics"
+	"fmt"
+	"time"
+	"sync"
 )
 
 func main() {
-	basics.RunBasics()
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go printNumbers(i, &wg)
+	}
+	wg.Wait()
+}
+
+
+func printNumbers(i int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(time.Duration(1) * time.Second)
+	fmt.Println(i)
 }
