d79cac288aff081572c15d5b0a0aab36f2bf4184
Krishnakumar Chandran <mail2krishnachandran@gmail.com>
2024-10-18 21:54:34 -0700

    basic variables and conditions added

--- main.go
+++ main.go
@@ -6,5 +6,21 @@ import (
 
 func main() {
 	fmt.Println("Hello, World!")
+	var age int = 30
+	var name string = "Krishna"
+	var height float32 = 5.10
+	fmt.Printf("My name is %s and my age is %d\n",name, age)
+	fmt.Printf("My height is %.2f\n", height)
+
+	place := "Erode"
+	runs := 80
+	fmt.Println("I am from", place)
+
+	if runs > 100 {
+		fmt.Println("You have scored a century")
+	} else {
+		fmt.Println("You have scored less than a century")
+	}
+
 
 }
